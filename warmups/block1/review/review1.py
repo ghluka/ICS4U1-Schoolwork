c9bbb066ebe457cec140b6a5ce65a6f73f0e6896
Luka <164436043+ghluka@users.noreply.github.com>
2025-02-04 15:01:52 -0500

    New functions todo

--- warmups/block1/review/review1.py
+++ warmups/block1/review/review1.py
@@ -2,12 +2,12 @@ from random import choice, randrange
 
 
 def make_input_file(quantity: int, filename: str) -> None:
-    '''
+    """
     Creates a file called with the given 'filename' full of random
     quantities and prices for 'quantity' items, one per line.
     Sample line format: 57$16.27
-    '''
-    out_file = open(filename, 'w')
+    """
+    out_file = open(filename, "w")
 
     for i in range(quantity):
         q = randrange(1, 6) + randrange(0, 8) + choice([0, 0, 0, 0, 0, 0, 5, 11]) * 10
@@ -19,23 +19,23 @@ def make_input_file(quantity: int, filename: str) -> None:
     out_file.close()
 
 
-def avg_qty_per_product(qtys) -> float:
+def avg_qty_per_product(qtys:list[int]) -> float:
     """
-    Returns the average quantity per product
+    Return the average quantity per item.
     """
     return sum(qtys) / len(qtys)
 
 
-def qty_of_most_expensive(qtys, prices) -> int:
+def qty_of_most_expensive(qtys:list[int], prices:list[float]) -> int:
     """
-    Returns the quantity of the most expensive product
+    Returns the quantity in inventory of the most expensive product.
     """
     return qtys[prices.index(max(prices))]
 
 
-def total_inventory_value(qtys, prices) -> float:
+def total_inventory_value(qtys:list[int], prices:list[float]) -> float:
     """
-    Returns the total value of the inventory
+    Returns the total monetary value of all items in inventory.
     """
     value = 0
 
@@ -45,16 +45,16 @@ def total_inventory_value(qtys, prices) -> float:
     return value
 
 
-def avg_price_per_item(qtys, prices) -> float:
+def avg_price_per_item(qtys:list[int], prices:list[float]) -> float:
     """
-    Returns the average price per product
+    Returns average price per single inventory item.
     """
-    return sum(prices) / len(prices)
+    return total_inventory_value(qtys, prices) / sum(qtys)
 
 
-def median_price_per_product(prices) -> float:
+def median_price_per_product(prices:list[float]) -> float:
     """
-    Returns the median price per product
+    Returns average price per single inventory item.
     """
     prices.sort()
     median = 0
@@ -67,16 +67,56 @@ def median_price_per_product(prices) -> float:
     return median
 
 
-def pretty_inventory(qtys, prices) -> str:
-    widest_qty = len(str(max(qtys)))
-    widest_price = len(str(max(prices)))
-    
+def pretty_inventory(qtys:list[int], prices:list[float]) -> str:
+    """
+    Returns a large string showing all quantities and prices in the inventory,
+    one product per line with format: ".10 items @ $__2.91 each"
+    where the quantity is in 3 characters, price is in 6 characters
+    """
     inventory = ""
     
     for i in range(len(prices)):
-        inventory += f"| {qtys[i]:>{widest_qty}} | {prices[i]:<{widest_price}} |\n"
+        inventory += f"{qtys[i]:.>3} items @ ${prices[i]:_>6} each\n"
     
-    return inventory
+    return inventory[:-1]
+
+
+def most_valuable_product(qtys:list[int], prices:list[float]):
+    """
+    Returns a string showing the quantity and price of the product that 
+    contains the most value in the inventory (based on its price & quantity).
+    The returned string is in the format ".10 items @ $__2.91 each"
+    where the quantity is in 3 characters, price is in 6 characters.
+    """
+    qty = qty_of_most_expensive(qtys, prices)
+    price = prices[qtys.index(qty_of_most_expensive(qtys, prices))]
+    
+    return f"{qty:.>3} items @ ${price:_>6} each"
+
+
+def products_in_price_range(prices:list[float], end1, end2):
+    """
+    Returns the number of products that have a price between end1 and end2 (inclusive).
+    """
+
+
+def distinct_prices(prices:list[float]): 
+    """
+    Returns a list of all distinct price amounts in inventory (i.e., with no
+    duplicates), in order from lowest price to highest price.
+    """
+
+
+def price_counts(prices:list[float]):
+    """
+    Returns a large string showing all distinct prices and the number
+    of products that have that price.
+    For example:
+       $__9.45: 1 product
+       $_11.45: 5 products
+       $_25.23: 2 products
+       etc...
+    """
 
 
 if __name__ == "__main__":
@@ -103,4 +143,8 @@ if __name__ == "__main__":
     print(f"Total inv. value: ${total_inventory_value(qtys, prices):.2f}")
     print(f"Avg $ per item: ${avg_price_per_item(qtys, prices):.2f}")
     print(f"Median $ per prod: ${median_price_per_product(prices):.2f}")
-    print(pretty_inventory(qtys, prices))
+    #print(pretty_inventory(qtys, prices))
+    print(f"Most valuable product: ${most_valuable_product(prices)}")
+    ####!products_in_price_range
+    print(f"Most valuable product: ${distinct_prices(prices)}")
+    print(f"Most valuable product: ${price_counts(prices)}")
