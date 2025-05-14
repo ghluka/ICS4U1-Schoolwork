6e0283dbafde86cb9cc7c90353635c296bedbd23
Luka <164436043+ghluka@users.noreply.github.com>
2025-05-14 14:29:12 -0400

    BST tester & __tail LL impl.

--- tasks/task3/t3_bst.py
+++ tasks/task3/t3_bst.py
@@ -28,4 +28,34 @@ class BSTree(utils.bst.BSTree):
         return NotImplemented
 
 if __name__ == "__main__":
-    pass
+    test = BSTree()
+    test.insert(8)
+    test.insert(3)
+    test.insert(1)
+    test.insert(6)
+    test.insert(4)
+    test.insert(7)
+    test.insert(10)
+    test.insert(14)
+    test.insert(13)
+    test.print_tree()
+
+    print("=-- index() test --=")
+    print("Index of 0:", test.index(0)) # -1
+    print("Index of 0:", test.index(6)) # 3
+
+    print("=-- average_depth() test --=")
+    print(test.average_depth()) # 2.75
+
+    print("=-- ordered_list() test --=")
+    print(test.ordered_list()) # [1, 3, 4, 6, 7, 8, 10, 13, 14]
+
+    print("=-- values_at_depth() test --=")
+    print("Values at depth 2:", test.values_at_depth(2)) # [1, 6, 14]
+
+    print("=-- longest_path() test --=")
+    print(test.longest_path()) # [8,3,6,4], [8,3,6,7], [8,10,14,13]
+
+    print("=-- optimize() test --=")
+    test.optimize()
+    test.print_tree()
--- tasks/task3/t3_linked_list.py
+++ tasks/task3/t3_linked_list.py
@@ -7,9 +7,56 @@ from typing import Self
 sys.path[0] = os.getcwd()
 
 import utils.linked_list
+from utils.linked_list import List_Node
 
 
 class Linked_List(utils.linked_list.Linked_List):
+    # Tail implementation
+    def __init__(self):
+        super().__init__()
+        self.__tail = None
+
+    def insert(self, value: object) -> None:
+        '''
+        Inserts the given value at the front of this linked list.
+        '''
+        self.__head = List_Node(value, self.__head)
+        if self.__head.get_next() == None:
+            self.__tail = self.__head
+
+    def insert_at_end(self, value: object) -> None:
+        '''
+        Inserts the given value at the end of this linked list.
+        '''
+        # wrap the new value in a Node
+        new_node = List_Node(value, None)  # next is None since it is the last
+
+        # we need to handle inserting the FIRST item in an empty list
+        if self.__head == None:
+            self.__head = new_node
+            self.__tail = new_node
+
+        else:
+            self.__tail.set_next(new_node)
+            self.__tail = new_node
+
+    def extend(self, other_list: Self) -> None:
+        '''
+        Extends this linked list by adding the other_list to the end.
+        '''
+        self.__tail.set_next(other_list.__head)
+        self.__tail = other_list.__tail
+
+    def reverse(self) -> None:
+        '''
+        Reverses the Linked_List so that it contains all the same data values,
+        but in reversed order.  This mutates the linked list.
+        '''
+        tail = self.__head
+        super().reverse()
+        self.__tail = tail
+    
+    # Functions
     def get_largest(self) -> object:
         return NotImplemented
 
