e7fa1bd0f77f3c62dafe7bb710505c22c87dc7b2
ghluka <164436043+ghluka@users.noreply.github.com>
2025-05-15 13:09:55 -0400

    Update t3_linked_list.py

--- tasks/task3/t3_linked_list.py
+++ tasks/task3/t3_linked_list.py
@@ -47,6 +47,65 @@ class Linked_List(utils.linked_list.Linked_List):
         self.__tail.set_next(other_list.__head)
         self.__tail = other_list.__tail
 
+    def remove(self, value: object) -> None:
+        '''
+        Removes the first occurrence of the specified object from the
+        Linked_List, if present.
+        '''
+        if self.__head == None:
+            #empty list case
+            self.__tail = None
+            return
+
+        elif self.__head.get_data() == value:
+            #first item case, where head of list needs updating
+            self.__head = self.__head.get_next()
+            if self.__head == None:
+                self.__tail = None
+
+        else:
+            #Trail a 2nd reference to simplify deletion
+            previous = self.__head
+            current = self.__head.get_next()
+            while current != None and current.get_data() != value:
+                previous = current
+                current = current.get_next()
+            if current == None: #item doesnt exist
+                return
+            else:
+                previous.set_next(current.get_next())
+                if previous.get_next() == None:
+                    self.__tail = previous
+
+    def insert_at(self, index: int, value: object) -> None:
+        '''
+        Inserts the given value at the given index, shifting all all
+        other index values up by one.  For example, lst.insert_at(3, 99)
+        would insert the data value 99 at index 3.
+        '''
+        #handle the head insert separately
+        if index == 0:
+            self.insert(value)
+        else:
+            #traverse to node *before* where the insert should happen
+            current_i = 1
+            current = self.__head
+        
+            #traverse to the desired index (or off end of list)
+            while current != None and current_i != index:
+                current = current.get_next()
+                current_i += 1
+            
+            #if the index was actually found (we didnt go off end of list)
+            #then insert here
+            if current != None:
+                new_node = List_Node(value, current.get_next())
+                current.set_next(new_node)
+                if new_node.get_next() == None:
+                    self.__tail = current
+
+            #else: ignore the insert... its an invalid index
+
     def reverse(self) -> None:
         '''
         Reverses the Linked_List so that it contains all the same data values,
